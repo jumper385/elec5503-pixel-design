166c15faf7fd9458404cb0a79baec83abee67e68
henry <henrychen385@gmail.com>
2025-04-05 09:59:46 +0000

    added shell config

--- common.sh
+++ common.sh
@@ -5,7 +5,17 @@ function setup_env() {
     ENV_FILE=".env"
     if [ ! -f "$ENV_FILE" ]; then
         echo "Error: $ENV_FILE file not found!"
-        exit 1
+
+        # prompt for username
+        read -p "Enter your username: " USERNAME
+        HOSTNAME=172.105.254.131
+        REPO_NAME=elec5503-remote
+
+        # create .env file
+        echo "REMOTE_USER=$USERNAME" > $ENV_FILE
+        echo "REMOTE_HOST=$HOSTNAME" >> $ENV_FILE
+        echo "REPO_NAME=$REPO_NAME" >> $ENV_FILE
+
     fi
 
     for line in $(cat $ENV_FILE | grep -v '^#' | xargs); do
