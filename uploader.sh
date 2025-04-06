014eae4a526c3d2c86f0ab7e611904f09f5ec4b7
henry <henrychen385@gmail.com>
2025-04-06 05:01:46 +0000

    fixed pathing issue

--- uploader.sh
+++ uploader.sh
@@ -41,22 +41,24 @@ if [ -z "$REPO_NAME" ]; then
     exit 1
 fi
 
+FILENAME=${REPO_NAME}_${REMOTE_USER}
+
 # Upload from directory
 if [ "$MODE" == "upload" ]; then
     # zip the current directory
-    tar -czf /tmp/$REPO_NAME.tar.gz --no-xattrs --exclude='.git' --exclude 'ssh_key*' .
+    tar -czf /tmp/$FILENAME.tar.gz --no-xattrs --exclude='.git' --exclude 'ssh_key*' .
     # upload to remote server
-    scp -i ./ssh_key_$REMOTE_USER /tmp/$REPO_NAME.tar.gz $REMOTE_USER@$REMOTE_HOST:/tmp
+    scp -i ./ssh_key_$REMOTE_USER /tmp/$FILENAME.tar.gz $REMOTE_USER@$REMOTE_HOST:/tmp
     # unzip the file on remote server
     ssh -i ./ssh_key_$REMOTE_USER $REMOTE_USER@$REMOTE_HOST "mkdir -p ~/$REPO_NAME"
-    ssh -i ./ssh_key_$REMOTE_USER $REMOTE_USER@$REMOTE_HOST "tar -xf /tmp/$REPO_NAME.tar.gz -C ~/$REPO_NAME"
+    ssh -i ./ssh_key_$REMOTE_USER $REMOTE_USER@$REMOTE_HOST "tar -xf /tmp/$FILENAME.tar.gz -C ~/$REPO_NAME"
 fi
 
 # Download from directory
 if [ "$MODE" == "download" ]; then
     # download the current directory from the remote server
     # ignore .git directory
-    ssh -i ./ssh_key_$REMOTE_USER $REMOTE_USER@$REMOTE_HOST "tar -czf /tmp/$REPO_NAME.tar.gz --exclude .git --exclude 'ssh_key*' $REPO_NAME"
-    scp -i ./ssh_key_$REMOTE_USER $REMOTE_USER@$REMOTE_HOST:/tmp/$REPO_NAME.tar.gz ../
-    tar -xf ../$REPO_NAME.tar.gz -C ../
+    ssh -i ./ssh_key_$REMOTE_USER $REMOTE_USER@$REMOTE_HOST "tar -czf /tmp/$FILENAME.tar.gz --exclude .git --exclude 'ssh_key*' $REPO_NAME"
+    scp -i ./ssh_key_$REMOTE_USER $REMOTE_USER@$REMOTE_HOST:/tmp/$FILENAME.tar.gz ../
+    tar -xf ../$FILENAME.tar.gz -C ../
 fi
